d4bd8470098428bc7a8a4a03b1e31ddecd3b4933
Iury Prates @RControl <ti@rcontrolbr.com.br>
2026-01-19 16:48:39 -0400

    add server.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { buscaEmpresa } from "./services/buscaEmpresa.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/empresa/:cnpj", async (requestAnimationFrame, res) => {
+    try{
+        const { cnpj } = requestAnimationFrame.params;
+        const dados = await buscaEmpresa(cnpj);
+        res.json(dados);
+    }catch(error){
+        res.status(400).json({
+            erro: "Erro ao buscar CNPJ",
+            detalhe: error.message
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Listen in port ${PORT}`);
+});
